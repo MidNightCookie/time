b0a34d82fb708ffca210befea2a61feb33963c9e
John Cook <cooks.johns@gmail.com>
2018-04-12 14:16:08 -0500

    html is set next will work on the functions of the page

---
+++ js/simon.js
@@ -0,0 +1,46 @@
+'use strict';
+//------------================      STYLE
+$('body').css({
+    'background-color': '',
+    'color': 'white',
+    'font-family': 'Muli, sans-serif'
+});
+
+$('.gamefield').css({
+    'max-width': '480px',
+    'justify-content': 'center'
+});
+
+$('.align').css({
+    'display': 'flex',
+    'justify-content': 'center'
+});
+
+$('.top-row').css({
+    '.align': '0'
+});
+
+$('.middle-row').css({
+    'display': 'flex',
+
+});
+
+$('').css({});
+
+$('').css({});
+
+$('').css({});
+
+$('').css({});
+
+$('').css({});
+
+$('').css({});
+
+$('').css({});
+
+$('').css({});
+$('').css({});
+$('').css({});
+
+///------------================     ACTIONS
